8d88f1dc17f2297a842b4f4412a0b3969ee0366e
Paulo <paulohenriquesms10@gmail.com>
2023-01-19 15:39:49 -0300

    feat: finish header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import './styles/global.css';
-
+import { Header } from './components/Header';
 // import { Habit } from './components/Habit'
 
 export function App() {
   return (
-    <div>
-
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-5xl px-6 flex flex-col gap-16">
+        
+        <Header/>
+      
+      </div>
     </div>
   )
 }
