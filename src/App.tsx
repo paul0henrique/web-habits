1067a58965eea151ead2c62997dfda00cc405963
Paulo <paulohenriquesms10@gmail.com>
2023-01-19 15:15:16 -0300

    feat: update structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,11 @@
 import './styles/global.css';
 
-import { Habit } from './components/Habit'
+// import { Habit } from './components/Habit'
 
-function App() {
+export function App() {
   return (
     <div>
-      <Habit completed={3} />
-      <Habit completed={4}/>
-      <Habit completed={5}/>
+
     </div>
   )
 }
-
-export default App
